b6335c5e47670be6f8a275e75f7c51302dcdbe18
Adham <admuaq@gmail.com>
2018-07-19 21:26:12 +0100

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+      t = self.split(/\.|!!|\?/)
+      #Use of regex
+      # - see https://bneijt.nl/pr/ruby-regular-expressions/
+      # http://rubylearning.com/satishtalim/ruby_regular_expressions.html
+      t.count
   end
 end
