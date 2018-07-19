203d5f36ccf2c9d0856e7e506125ae0fd2a718bd
Adham Muhammad <admuaq@gmail.com>
2018-07-19 15:06:23 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+      t = self.split(/[\.\?!]/)
+      t.count
   end
 end
+      binding.pry
+      
+      puts = "asdsadsa"
